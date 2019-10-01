bacfe7c8f3d4e701d03ed84713c7ebcb3d332706
Anhelina Lakhtanava <linobanana@gmail.com>
2019-10-01 17:23:03 +0300

    need to fix

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arrayFromString = [];
+  arrayFromString = Array.from(str);
+  let brackets = [].concat(...bracketsConfig);
+  let stack = [];
+
+  if (arrayFromString.length % 2 != 0) {
+    return false;
+  }
+
+  for (let i = 0; i < arrayFromString.length; i++) {
+    for (let j = 0; j < brackets.length; j++) {
+      if (arrayFromString[i] === brackets[j] && (j === 0 || j % 2 === 0)) {
+        stack.push(arrayFromString[i]);
+      } else if (brackets[j] === brackets[j + 1] || brackets[j] === brackets[j - 1]) {
+          if (arrayFromString[i] === brackets[j] && j % 2 !== 0 && i % 2 !== 0 && stack[stack.length - 1] === brackets[j - 1]) {
+            stack.pop();
+          } else {
+            continue;
+          }
+      } else if (arrayFromString[i] === brackets[j] && j % 2 !== 0 && stack[stack.length - 1] === brackets[j - 1]) {
+        stack.pop();
+      }
+      }
+    }
+
+  if (stack.length === 0) {
+    return true;
+  } else {
+  return false;
+  }
 }
